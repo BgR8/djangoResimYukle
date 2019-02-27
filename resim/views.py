9da0f4c62f77c433ca5c96ed2849709be3d1e49f
BgR8 <bugra.argon@gmail.com>
2019-02-27 14:09:23 +0200

    resimler veritabanına kaydediliyor

--- resim/views.py
+++ resim/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import Resim
+from .forms import PostForm
+from PIL import Image
 
 # Create your views here.
 def index(request):
@@ -7,11 +9,17 @@ def index(request):
     return render(request, 'index.html', {'resimler': resimler})
 
 def resimEkle(request):
-    if request.method == "GET":
-        return redirect('/')
-    else:
-        title = request.POST.get('title')
-        newImg = Resim(title = title)
+    #if request.method == "GET":
+    #    return redirect('/')
+    #else:
+    #    title = request.POST.get('title')
+    #    newImg = Resim(title = title)
 
-        newImg.save()
+    #    newImg.save()
+    #    return redirect('/')
+
+    form = PostForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.save()
         return redirect('/')
